77950628ba099fcad87083636d7dd57b47ce8c8a
adachsoft <adach_soft@wp4b.pl>
2018-08-17 06:53:56 +0200

    install 16.04

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #/bin/bash
 
+#for PHP 7.2
+sudo add-apt-repository -y ppa:ondrej/php
+
 sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install apache2 -y
 sudo apt-get install php7.2 -y
 sudo apt-get install nmap -y
 sudo apt-get install php7.2-mysql -y
+sudo apt-get install php7.2-pgsql -y
+sudo apt-get install libapache2-mpm-itk -y
+
+#nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs -y
+
+
